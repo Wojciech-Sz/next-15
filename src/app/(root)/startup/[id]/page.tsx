2d9c3c10554e199b68864f6b2962d4e7266a165c
WojciechSz <w.szczygielski00@gmail.com>
2024-11-29 11:32:08 +0100

    playlist

--- src/app/(root)/startup/[id]/page.tsx
+++ src/app/(root)/startup/[id]/page.tsx
@@ -1,12 +1,16 @@
 import React, { Suspense } from "react";
 import Link from "next/link";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import { formatDate } from "@/lib/utils";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 
 const md = markdownit();
 export const experimental_ppr = true;
@@ -18,9 +22,14 @@ const Page = async ({
 }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
-  const parsedContent = md.render(post?.pitch || "");
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "elo",
+    }),
+  ]);
 
+  const parsedContent = md.render(post?.pitch || "");
   if (!post) notFound();
 
   return (
@@ -75,6 +84,17 @@ const Page = async ({
         </div>
 
         <hr className={"divider"} />
+
+        {editorPosts.length > 0 && (
+          <div className={"max-w-4xl mx-auto"}>
+            <p className={"text-30-semibold"}>Editor Picks</p>
+            <ul className={"mt-7 card_grid-sm"}>
+              {editorPosts.map((post: StartupCardType) => (
+                <StartupCard key={post?._id} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
 
         <Suspense fallback={<Skeleton className={"view_skeleton"} />}>
           <View id={id} />
